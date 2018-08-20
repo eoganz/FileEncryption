7b1fe95254082bbbe9aa902a16cde325bd8bd526
Jake Mettler <36274709+eoganz@users.noreply.github.com>
2018-08-20 16:40:34 -0700

    Add files via upload

---
+++ FileIO/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FileIO
+{
+    partial class FileEncrypter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.rtbFile = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(140, 56);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import File";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(187, 12);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(140, 56);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(363, 12);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(140, 56);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // rtbFile
+            // 
+            this.rtbFile.Location = new System.Drawing.Point(12, 160);
+            this.rtbFile.Name = "rtbFile";
+            this.rtbFile.Size = new System.Drawing.Size(491, 278);
+            this.rtbFile.TabIndex = 3;
+            this.rtbFile.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(187, 84);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 56);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // FileEncrypter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.rtbFile);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnImport);
+            this.Name = "FileEncrypter";
+            this.Text = "File Encryption";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.RichTextBox rtbFile;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FileIO/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace FileIO
+{
+    public partial class FileEncrypter : Form
+    {
+        public FileEncrypter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = GetFilePath();
+            file.Filter = "Text File|*.txt";
+
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                rtbFile.Clear();
+                StringBuilder data = new StringBuilder();
+                data.Append(File.ReadAllText(file.FileName).ToString());
+
+                rtbFile.AppendText(data.ToString());
+            }
+        }
+
+        private string GetFilePath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            StringBuilder contents = new StringBuilder();
+            contents.AppendLine(rtbFile.Text);
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Data File|*.txt";
+            save.RestoreDirectory = true;
+
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                string path = save.FileName;
+                if (path == string.Empty)
+                    return;
+                File.WriteAllText(path, contents.ToString());
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            StringBuilder contents = new StringBuilder();
+            contents.Append(rtbFile.Text);
+            string contentsToString = contents.ToString();
+            rtbFile.Clear();
+            rtbFile.Text = RijndaelSimple.Decrypt(contentsToString, "Bananas", "LeagueOfLegends", "SHA1", 2, "1234567890123456", 192);
+
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            StringBuilder contents = new StringBuilder();
+            contents.Append(rtbFile.Text);
+            string contentsToString = contents.ToString();
+            rtbFile.Clear();
+            rtbFile.Text = RijndaelSimple.Encrypt(contentsToString, "Bananas", "LeagueOfLegends", "SHA1", 2, "1234567890123456", 192);
+        }
+    }
+}
